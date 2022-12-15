da73c6f81f35bdfb2b2458c282f0b0d327699a4b
JulieKodehode <julie.kodehode@gmail.com>
2022-12-15 14:23:07 +0100

    Added basic footer page and css file

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+// Libraries
+import React from "react";
+
+// Styles
+import "./layout.module.css";
+
+// Components
+
+/**
+ * This is the main Footer
+ */
+const Footer = () => {
+	return (
+		<footer>
+			<h2>Thanks for visiting</h2>
+		</footer>
+	);
+};
+
+export default Footer;
